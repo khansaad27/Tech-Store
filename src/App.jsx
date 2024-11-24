aadd87eb15de39d46da1e3de053550f45e149dba
Saad_khan <164928186+khansaad27@users.noreply.github.com>
2024-11-25 02:39:03 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,13 @@ import {
   RouterProvider,
 } from 'react-router-dom';
 import RootLayOut from './component/RootLayOut';
-// import ProductListPage from './layer/ProductCard/ProductListPage ';
+
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<RootLayOut />}>
       <Route index element={<Home />} />
-      {/* <Route path="/products" element={<ProductListPage products={products} />} /> */}
+
     </Route>
   )
 );
